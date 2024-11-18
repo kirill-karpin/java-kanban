c928cdfd41c62942e9f7d750b2a4f1a38fb3d7ea
Карпин Кирилл <kirill_karpin@hotmail.com>
2024-11-18 12:06:10 +0500

    CodeStyle check

--- src/main/java/com/tracker/FileBackedTaskManager.java
+++ src/main/java/com/tracker/FileBackedTaskManager.java
@@ -11,7 +11,6 @@ import java.util.List;
 public class FileBackedTaskManager extends InMemoryTaskManager {
 
     private static String FILE_NAME = "database.csv";
-    private char CSV_DELIMITER = ';';
     private boolean fileInitialized = true;
 
     public FileBackedTaskManager(HistoryManager historyManager) {
@@ -22,13 +21,17 @@ public class FileBackedTaskManager extends InMemoryTaskManager {
         return FILE_NAME;
     }
 
+    public static String getDelimiter() {
+        return ";";
+    }
+
     public List<String[]> parseFile(File file) {
         List<String[]> tasks = new ArrayList<>();
 
         try (BufferedReader br = new BufferedReader(new FileReader(file))) {
             while (br.ready()) {
                 String line = br.readLine();
-                tasks.add(line.split(String.valueOf(CSV_DELIMITER)));
+                tasks.add(line.split(getDelimiter()));
             }
             return tasks;
         } catch (IOException e) {
@@ -80,7 +83,7 @@ public class FileBackedTaskManager extends InMemoryTaskManager {
             item.add(data.getOrDefault(key, ""));
         }
 
-        return String.join(String.valueOf(CSV_DELIMITER), item);
+        return String.join(getDelimiter(), item);
     }
 
     public String serialize(String[] header) {
@@ -116,7 +119,7 @@ public class FileBackedTaskManager extends InMemoryTaskManager {
     public void save() {
         String[] header = {"id", "type", "name", "status", "description", "epic"};
         String csvData = serialize(header);
-        String data = String.join(String.valueOf(CSV_DELIMITER), header) + "\n" +
+        String data = String.join(getDelimiter(), header) + "\n" +
                 csvData;
         saveToFile(getFile(), data);
     }
